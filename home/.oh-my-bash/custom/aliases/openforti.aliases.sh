5de6790552288b124905d59c8e1578c7bff8f374
Ali Mehraji <a.mehraji75@gmail.com>
2026-07-15 22:47:55 +0330

    remove: the openforti alias file
    Signed-off-by: Ali Mehraji <a.mehraji75@gmail.com>

--- home/.oh-my-bash/custom/aliases/openforti.aliases.sh
+++
@@ -1,2 +0,0 @@
-alias bmbzr-openforti-all="sudo openfortivpn -c /etc/openfortivpn/bmbzr-forti-all"
-alias bmbzr-openforti-sindad-restructure="sudo openfortivpn -c /etc/openfortivpn/bmbzr-sindad-restructure"
